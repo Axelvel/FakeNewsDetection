e63a887c5d46e4a8583bcdbf8af483155b9f5fdb
Axel Velez <axelvel@live.fr>
2023-12-19 22:57:54 +0800

    :sparkles: Added columns

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import torch as tf
 import pandas as pd
+from model import FakeNewsClassifier
 
 DATASET = 'liar_dataset/'
 TRAIN_PATH = DATASET + 'train.tsv'
 TEST_PATH = DATASET + 'test.tsv'
 EVAL_PATH = DATASET + 'valid.tsv'
 
-df = pd.read_csv(TRAIN_PATH, sep='\t')
+COLUMNS = ['id', 'label', 'statement', 'subject', 'speaker', 'job', 'state', 'party', 'barely-true', 'false', 'half-true', 'mostly-true', 'pants-on-fire', 'context']
 
-print(df)
-print(df.shape)
+df_train = pd.read_csv(TRAIN_PATH, sep='\t')
+df_test = pd.read_csv(TEST_PATH, sep='\t')
+df_eval = pd.read_csv(EVAL_PATH, sep='\t')
+
+# Setting the columns
+df_train.columns = COLUMNS
+df_test.columns = COLUMNS
+df_eval.columns = COLUMNS
+
+# Data preprocessing
+df_train.pop('id')
+
+print(df_train)
+print(df_train.shape)
