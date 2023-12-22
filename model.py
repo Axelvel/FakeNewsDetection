64a3ea59f47ede986794f0a0d5d2730a9c594de4
Axel Velez <axelvel@live.fr>
2023-12-22 22:27:32 +0800

    :sparkles: Updated model architecture

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from transformers import AutoModel
 
@@ -5,10 +6,32 @@ from transformers import AutoModel
 BERT_MODEL = "bert-base-uncased"
 
 class FakeNewsClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, meta_hidden_size, hidden_size, output_size):
         super(FakeNewsClassifier, self).__init__()
 
-        self.bert = AutoModel.from_pretrained(BERT_MODEL)
+        self.bert_hidden_size = 768
+        self.dropout_rate = 0.5
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.meta_hidden_size = meta_hidden_size
 
-    def forward(self, x, meta):
-        return x
+
+        self.bert = AutoModel.from_pretrained(BERT_MODEL)
+        self.metadata_fc = nn.Linear(self.meta_hidden_size, self.hidden_size)
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.bert_hidden_size + self.meta_hidden_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Dropout(self.dropout_rate),
+            nn.Linear(self.hidden_size, self.output_size)
+        )
+
+
+
+    def forward(self, inputs, metadatas, attention_mask):
+        bert_output = self.bert(inputs, attention_mask=attention_mask)['last_hidden_state']
+        bert_pooled_output = torch.mean(bert_output, dim=1)
+        metadata_output = self.metadata_fc(metadatas)
+        concatenated_output = torch.cat([bert_pooled_output, metadata_output], dim=1)
+        logits = self.fc(concatenated_output)
+        return logits
