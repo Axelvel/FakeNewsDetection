b072ff3e0b4333dd6c21844a572f91896bda15ea
Axel Velez <axelvel@live.fr>
2023-12-22 23:29:34 +0800

    :sparkles: Added label encoding

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+import joblib
 
 DATASET = 'liar_dataset/'
 TRAIN_PATH = DATASET + 'train.tsv'
@@ -18,15 +20,20 @@ df_eval = pd.read_csv(EVAL_PATH, sep='\t', names=COLUMNS)
 df_train.pop('id')
 
 # Displaying histogram
-
 label_distribution = []
 for label in LABELS:
     label_distribution.append(np.count_nonzero(df_train['label'] == label))
 
 print(label_distribution)
-
 plt.bar(LABELS, label_distribution)
 plt.show()
+
+# Encoding labels
+label_encoder = LabelEncoder()
+label_encoder.fit(LABELS)
+df_train['label'] = label_encoder.transform(df_train['label'])
+joblib.dump(label_encoder, 'data/label_encoder.plk')
+
 
 print(df_train)
 print(df_train.shape)
