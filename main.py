50c4189be5a24be1cd4368cb20e9b0015dd55d54
Clamorae <trombiniquentin@gmail.com>
2023-12-22 16:57:38 +0800

    :bar_chart: Added histogram chart

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-import torch as tf
+import numpy as np
 import pandas as pd
-from model import FakeNewsClassifier
+import matplotlib.pyplot as plt
 
 DATASET = 'liar_dataset/'
 TRAIN_PATH = DATASET + 'train.tsv'
 TEST_PATH = DATASET + 'test.tsv'
 EVAL_PATH = DATASET + 'valid.tsv'
 
-COLUMNS = ['id', 'label', 'statement', 'subject', 'speaker', 'job', 'state', 'party', 'barely-true', 'false', 'half-true', 'mostly-true', 'pants-on-fire', 'context']
+COLUMNS = ['id', 'label', 'statement', 'subject', 'speaker', 'job', 'state', 'party', 'barely-true', 'false', 'half-true', 'mostly-true', 'pants-fire', 'context']
+LABELS = ['pants-fire', 'false', 'barely-true', 'half-true', 'mostly-true', 'true']
 
-df_train = pd.read_csv(TRAIN_PATH, sep='\t')
-df_test = pd.read_csv(TEST_PATH, sep='\t')
-df_eval = pd.read_csv(EVAL_PATH, sep='\t')
-
-# Setting the columns
-df_train.columns = COLUMNS
-df_test.columns = COLUMNS
-df_eval.columns = COLUMNS
+df_train = pd.read_csv(TRAIN_PATH, sep='\t', names=COLUMNS)
+df_test = pd.read_csv(TEST_PATH, sep='\t', names=COLUMNS)
+df_eval = pd.read_csv(EVAL_PATH, sep='\t', names=COLUMNS)
 
 # Data preprocessing
 df_train.pop('id')
+
+# Displaying histogram
+
+label_distribution = []
+for label in LABELS:
+    label_distribution.append(np.count_nonzero(df_train['label'] == label))
+
+print(label_distribution)
+
+plt.bar(LABELS, label_distribution)
+plt.show()
 
 print(df_train)
 print(df_train.shape)
