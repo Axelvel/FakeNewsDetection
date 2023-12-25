4ec418928ae8acbaa9eff452953713188b392461
Axel Velez <axelvel@live.fr>
2023-12-25 15:35:06 +0800

    :bug: Fixed model to work with data preprocessing

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
-import os
 import preprocessing
 import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 import joblib
+from model import FakeNewsClassifier
+from torch.utils.data import TensorDataset, DataLoader
 
 DATASET = 'liar_dataset/'
 TRAIN_PATH = DATASET + 'train.tsv'
@@ -16,8 +16,8 @@ COLUMNS = ['id', 'label', 'statement', 'subject', 'speaker', 'job', 'state', 'pa
 LABELS = ['pants-fire', 'false', 'barely-true', 'half-true', 'mostly-true', 'true']
 
 df_train = pd.read_csv(TRAIN_PATH, sep='\t', names=COLUMNS)
-df_test = pd.read_csv(TEST_PATH, sep='\t',names=COLUMNS)
-df_eval = pd.read_csv(EVAL_PATH, sep='\t',names=COLUMNS)
+df_test = pd.read_csv(TEST_PATH, sep='\t', names=COLUMNS)
+df_eval = pd.read_csv(EVAL_PATH, sep='\t', names=COLUMNS)
 
 # Data preprocessing
 df_train.pop('id')
@@ -28,9 +28,11 @@ all_topic, nb_topic_max = preprocessing.extract_topic(df_train)
 all_speaker = list(set(df_train["speaker"].to_list()))
 all_job = list(set(df_train["job"].to_list()))
 
-train_sentences, train_meta_data, train_mask = preprocessing.preprocess(df_train,all_topic, nb_topic_max,all_speaker,all_job)
-eval_sentences, eval_meta_data, eval_mask = preprocessing.preprocess(df_eval,all_topic, nb_topic_max,all_speaker,all_job)
-test_sentences, test_meta_data, test_mask = preprocessing.preprocess(df_test,all_topic, nb_topic_max,all_speaker,all_job)
+train_sentences, train_meta_data, train_mask = preprocessing.preprocess(df_train, all_topic, nb_topic_max, all_speaker, all_job)
+eval_sentences, eval_meta_data, eval_mask = preprocessing.preprocess(df_eval, all_topic, nb_topic_max, all_speaker, all_job)
+test_sentences, test_meta_data, test_mask = preprocessing.preprocess(df_test, all_topic, nb_topic_max, all_speaker, all_job)
+
+train_meta_data = train_meta_data.float()
 
 # Displaying histogram
 label_distribution = []
@@ -39,7 +41,7 @@ for label in LABELS:
 
 print(label_distribution)
 plt.bar(LABELS, label_distribution)
-#plt.show()
+# plt.show()
 
 # Encoding labels
 label_encoder = LabelEncoder()
@@ -47,6 +49,22 @@ label_encoder.fit(LABELS)
 df_train['label'] = label_encoder.transform(df_train['label'])
 joblib.dump(label_encoder, 'data/label_encoder.plk')
 
-
 print(df_train)
 print(df_train.shape)
+
+META_SIZE = len(train_meta_data[0])
+META_HIDDEN_SIZE = 128
+HIDDEN_SIZE = 128
+OUTPUT_SIZE = len(LABELS)
+BATCH_SIZE = 32
+
+model = FakeNewsClassifier(meta_size=META_SIZE, meta_hidden_size=META_HIDDEN_SIZE, hidden_size=HIDDEN_SIZE, output_size=OUTPUT_SIZE)
+
+# Trainloader
+dataset = TensorDataset(train_sentences, train_meta_data, train_mask)
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+for inputs, metadatas, masks in dataloader:
+    outputs = model(inputs, metadatas, masks)
+    print(outputs)
+    exit(0)
--- model.py
+++ model.py
@@ -6,18 +6,19 @@ from transformers import AutoModel
 BERT_MODEL = "bert-base-uncased"
 
 class FakeNewsClassifier(nn.Module):
-    def __init__(self, meta_hidden_size, hidden_size, output_size):
+    def __init__(self, meta_size, meta_hidden_size, hidden_size, output_size):
         super(FakeNewsClassifier, self).__init__()
 
         self.bert_hidden_size = 768
         self.dropout_rate = 0.5
         self.hidden_size = hidden_size
         self.output_size = output_size
+        self.meta_size = meta_size
         self.meta_hidden_size = meta_hidden_size
 
 
         self.bert = AutoModel.from_pretrained(BERT_MODEL)
-        self.metadata_fc = nn.Linear(self.meta_hidden_size, self.hidden_size)
+        self.metadata_fc = nn.Linear(self.meta_size, self.meta_hidden_size)
 
         self.fc = nn.Sequential(
             nn.Linear(self.bert_hidden_size + self.meta_hidden_size, self.hidden_size),
@@ -25,7 +26,6 @@ class FakeNewsClassifier(nn.Module):
             nn.Dropout(self.dropout_rate),
             nn.Linear(self.hidden_size, self.output_size)
         )
-
 
 
     def forward(self, inputs, metadatas, attention_mask):
