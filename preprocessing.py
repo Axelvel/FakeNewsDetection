d998b14d3ced034b22b28711ae75b49355f7566e
Axel Velez <axelvel@live.fr>
2023-12-25 16:21:46 +0800

    :art: Improved structure and removed unecessary code

--- preprocessing.py
+++ preprocessing.py
@@ -1,37 +1,38 @@
 import torch  
 import numpy as np
-from transformers import BertTokenizer,BertModel
-import math
+from transformers import BertTokenizer
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased') 
-model = BertModel.from_pretrained("bert-base-uncased")
 
-#NOTE - Two way to do it:
-    # - transform everything to text and add it raw(litteraly add the category's name beofre to create some context)
-    # - normalize everything and find a way to sum everything up after embedding
+# NOTE - Two way to do it:
+    # - Transform everything to text and add it raw (litteraly add the category's name before to create some context)
+    # - Normalize everything and find a way to sum everything up after embedding
+
 
 def extract_topic(df):
     topic = df["subject"].to_list()
     nb_topic_max = 0
     all_topic = []
     for line in topic:
-        if isinstance(line,str) :
+        if isinstance(line, str):
             splitted = line.split(",")
-            if len(splitted)>nb_topic_max:
+            if len(splitted) > nb_topic_max:
                 nb_topic_max = len(splitted)
             for item in splitted:
                 all_topic.append(item)
-    return list(set(all_topic)),nb_topic_max
+    return list(set(all_topic)), nb_topic_max
 
-def avoid_nan(entry,entry_list):
+
+def avoid_nan(entry, entry_list):
     try:
-        value  = entry_list.index(entry)/len(entry_list)
+        value = entry_list.index(entry)/len(entry_list)
     except:
         value = 0
     return value
 
+
 def extract_data(matrix, all_topic, nb_topic_max, all_speaker, all_job, all_states):
-    
+
     statements = []
     meta_datas = []
     mask = []
@@ -39,8 +40,8 @@ def extract_data(matrix, all_topic, nb_topic_max, all_speaker, all_job, all_stat
     for entry in matrix:
 
         line_meta_datas = []
-        
-        #convert each subject into a number and normalize it + padd with 0
+
+        # Convert each subject into a number and normalize it + padd with 0
         if isinstance(entry[2],str):
             for item in entry[2].split(","):
                 if nb_topic_max-len(line_meta_datas) > 0 and item in all_topic:
@@ -48,11 +49,11 @@ def extract_data(matrix, all_topic, nb_topic_max, all_speaker, all_job, all_stat
             for i in range(nb_topic_max-len(line_meta_datas)):
                 line_meta_datas.append(0.0)
 
-        line_meta_datas.append(avoid_nan(entry[3],all_speaker))
-        line_meta_datas.append(avoid_nan(entry[4],all_job))
-        line_meta_datas.append(avoid_nan(entry[5],all_states))
+        line_meta_datas.append(avoid_nan(entry[3], all_speaker))
+        line_meta_datas.append(avoid_nan(entry[4], all_job))
+        line_meta_datas.append(avoid_nan(entry[5], all_states))
 
-        #transform political position into number
+        # Transform political position into number
         if entry[6] == "republican":
             line_meta_datas.append(-1)
         elif entry[6] == "democrat":
@@ -63,7 +64,7 @@ def extract_data(matrix, all_topic, nb_topic_max, all_speaker, all_job, all_stat
         try:
             sum_speech = int(entry[7]) + int(entry[8]) + int(entry[9]) + int(entry[10]) + int(entry[11])
         except:
-            sum_speech=0
+            sum_speech = 0
 
         for i in range(5):
             if sum_speech != 0:
@@ -76,10 +77,10 @@ def extract_data(matrix, all_topic, nb_topic_max, all_speaker, all_job, all_stat
         meta_datas.append(line_meta_datas)
 
         if isinstance(entry[12],str):
-            tok =tokenizer(entry[1] + entry[12],max_length=512, add_special_tokens=True, truncation=True, padding='max_length', return_tensors="pt")
+            tok = tokenizer(entry[1] + entry[12], max_length=512, add_special_tokens=True, truncation=True, padding='max_length', return_tensors="pt")
         else:
-            tok = tokenizer(entry[1],max_length=512, add_special_tokens=True, truncation=True, padding='max_length', return_tensors="pt")
-            
+            tok = tokenizer(entry[1], max_length=512, add_special_tokens=True, truncation=True, padding='max_length', return_tensors="pt")
+
         statements.append(tok['input_ids'])
         mask.append(tok['attention_mask'])
 
@@ -87,16 +88,16 @@ def extract_data(matrix, all_topic, nb_topic_max, all_speaker, all_job, all_stat
 
     statements = torch.cat(statements, dim=0)
     mask = torch.cat(mask, dim=0)
-    
-    return statements,meta_datas,mask
+
+    return statements, meta_datas, mask
 
 
-def preprocess(df,all_topic, nb_topic_max,all_speaker,all_job):
-    
+def preprocess(df, all_topic, nb_topic_max, all_speaker, all_job):
+
     df = df.values.tolist()
 
-    with open("./states.txt") as f:
+    with open("./states.txt", encoding="utf-8") as f:
         all_states = f.read()
     all_states = all_states.split()
 
-    return(extract_data(df,all_topic, nb_topic_max,all_speaker,all_job,all_states))
+    return (extract_data(df, all_topic, nb_topic_max, all_speaker, all_job, all_states))
